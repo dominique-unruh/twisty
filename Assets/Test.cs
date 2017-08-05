30cf5385b14da12d4c0baac49af5a7c938e5a3e5
Dominique Unruh <dominique@unruh.de>
2017-08-05 12:48:56 +0300

    First experiments

---
+++ Assets/Test.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	public Letter a;
+	public Letter b;
+	public Letter c;
+	public Letter d;
+
+	public float radius = 5;
+	public float wait = .5f;
+
+	IEnumerator test(){
+		while (true) {
+			Vector2 pos = Random.insideUnitCircle * radius;
+			Letter l = letters [Random.Range (0, letters.Length)];
+			l.gotoPos (pos.x, pos.y);
+			yield return new WaitForSeconds(wait);
+		}
+	}
+	IEnumerator test2(){
+		yield return new WaitForSeconds(wait);
+		a.gotoTarget (1);
+		yield return new WaitForSeconds(wait);
+		b.gotoTarget (2);
+		yield return new WaitForSeconds(wait);
+		c.gotoTarget (0);
+		yield return new WaitForSeconds(wait);
+		d.gotoSource (1);
+	}
+
+	private Letter[] letters;
+
+	// Use this for initialization
+	void Start () {
+		letters = new Letter[] { a,b,c,d };
+		StartCoroutine (test2 ());
+	}
+
+}
---
+++ Assets/letter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Letter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		updateLetter ();
+		targetPosition = transform.localPosition;
+		targets = GameObject.Find ("Targets").transform;
+		sources = GameObject.Find ("Sources").transform;
+	}
+	private Transform targets, sources;
+
+	public float speed = 1;
+
+	// Update is called once per frame
+	void Update () {
+		updateLetter ();
+
+		float delta = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, targetPosition, delta);
+	}
+
+	void OnValidate(){
+		updateLetter ();
+	}
+		
+	private Vector3 targetPosition;
+	public void gotoPos(float x, float y) {
+		targetPosition = new Vector3 (x,y,0);
+	}
+
+	public void gotoTarget(int i) {
+		Transform child = targets.GetChild (i);
+		gotoPos (child.position.x, child.position.y);
+	}
+
+	public void gotoSource(int i) {
+		Transform child = sources.GetChild (i);
+		gotoPos (child.position.x, child.position.y);
+	}
+
+	public char letter = 'A';
+	public UnityEngine.U2D.SpriteAtlas spriteAtlas;
+	public Sprite errorSprite;
+
+	void updateLetter () {
+		if (letter != previousLetter) {
+			SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer> ();
+			spriteRenderer.sprite = spriteAtlas.GetSprite(letter.ToString());
+			if (spriteRenderer.sprite==null) spriteRenderer.sprite = errorSprite;
+			previousLetter = letter;
+		}
+	}
+
+
+	private char previousLetter='\x00';
+}
