cb6119ad883b816ead3d1b9372cb0d67ac2e4454
msalura <30748262+msalura@users.noreply.github.com>
2017-08-05 18:52:33 +0300

    Added backspace!

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -13,7 +13,7 @@ public class Controller : MonoBehaviour {
 	}
 
 	public const int numLetters = 6;
-	private List<Letter> lettersInTarget = new List<Letter>();
+	private LinkedList<Letter> lettersInTarget = new LinkedList<Letter>();
 	private Letter[] lettersInSource = new Letter[numLetters];
 
 	/** Moves all letters into the source positions */
@@ -25,12 +25,24 @@ public class Controller : MonoBehaviour {
 			lettersInSource [i].gotoSource (i);
 		}
 	}
+	void Backspace (){
+		if (lettersInTarget.Count == 0)
+			return;
+		Letter lastLetter = lettersInTarget.Last.Value;
+		lettersInTarget.RemoveLast ();
+		for (int i = 0; i < numLetters; i++)
+			if (lettersInSource [i] == null) {
+				lettersInSource [i] = lastLetter;
+				lastLetter.gotoSource (i);
+				break;
+			}
+	}
 	void LetterKeyPress (char key){
 		for (int i = 0; i < numLetters; i++) {
 			Letter letter = lettersInSource [i];
 			if (letter != null && letter.letter == key) {
 				lettersInSource [i] = null;
-				lettersInTarget.Add (letter);
+				lettersInTarget.AddLast (letter);
 				letter.gotoTarget (lettersInTarget.Count - 1);
 				break;
 			}
@@ -43,8 +55,10 @@ public class Controller : MonoBehaviour {
 				LetterKeyPress (key);
 			else if (key >= 'a' && key <= 'z')
 				LetterKeyPress (System.Char.ToUpper (key));
-			else 
-				print ("Unknown key: " + key);
+			else if (key == 8)
+				Backspace ();
+			else
+				print ("Unknown key: " + key+" "+System.Convert.ToInt32(key));
 		}
 	}
 }
